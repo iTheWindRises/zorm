5fbd794da0035e781fd845ba400abeb10b94bf26
wenjie <zhangwj@sucsoft.com>
2020-03-25 17:21:29 +0800

    更新删除

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"time"
 	"zorm"
 	"zorm/model"
 )
@@ -15,14 +17,47 @@ import (
 //	UNIQUE KEY `username` (`username`)
 //)
 
+var (
+	username string
+	password string
+	addr string
+	database string
+)
+
+func init()  {
+	flag.StringVar(&username,"u","root","数据库用户名")
+	flag.StringVar(&password,"p","root","数据库用户密码")
+	flag.StringVar(&addr,"addr","localhost:3306","数据库地址")
+	flag.StringVar(&database,"use","test","数据库名称")
+
+}
 
 func main() {
-	engine, _ := zorm.NewEngine("mysql", "root:rootroot@tcp(116.62.57.22:3306)/test?charset=utf8mb4&loc=Local&parseTime=true")
+	flag.Parse()
+	engine, _ := zorm.NewEngine("mysql",
+		fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&loc=Local&parseTime=true",username,password,addr,database))
 	defer engine.Close()
 	s := engine.NewSession()
 
 	//s.Insert(u1,u2)
-	var users []model.User
-	s.Where("username = ?","张三").Find(&users)
-	fmt.Println(users)
+	//var users []model.User
+	//s.Limit(2).Where("username like  ?","%1%").OrderBy("create_at desc").Find(&users)
+	//fmt.Println(users)
+
+	//var user model.User
+	//s.Where("username like  ?","%1%").First(&user)
+	//fmt.Println(user)
+	
+	// update
+	u1 := &model.User{
+		Uid:       4,
+		Username:  "爽歪歪",
+		Nickname:  "故事2",
+		Gender:    1,
+		CreatedAt: time.Now(),
+	}
+	//s.Save(u1)
+	//s.Update(u1, map[string]interface{}{"nickname":"故事3"})
+
+	s.Delete(u1)
 }
